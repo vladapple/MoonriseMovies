bff3bb3f81eca96327582b6de581072436b050ec
GermanSamosa <mat_rao@hotmail.com>
2022-11-06 11:18:25 -0500

    Working on index

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.EntityFrameworkCore;
+using MoonriseMovies.Data;
+using MoonriseMovies.Models;
 namespace MoonriseMovies.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private MoonriseDBContext db;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, MoonriseDBContext db)
     {
         _logger = logger;
+        this.db = db;
+    }
+    public List<Screening> Screenings {get; set;}
+    public List<Movie> Movies {get; set;}
+    public Movie FeaturedMovie {get; set;}
+    public async Task OnGetAsync()
+    {       
+        Movies = await db.Movies.ToListAsync();
+        FeaturedMovie = Movies.ElementAt(new Random().Next(Movies.Count));
     }
 
-    public void OnGet()
-    {
+   
 
-    }
 }
