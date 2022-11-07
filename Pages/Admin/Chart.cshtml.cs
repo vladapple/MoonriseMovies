be6f5e1bcfbd899cb5a5c4716f73df4c5e0b3c22
Vlad Epel <vladislav.epelboym@johnabbottcollege.net>
2022-11-07 14:12:16 -0500

    chart not finished

---
+++ Pages/Admin/Chart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MoonriseMovies.Pages.Admin
+{
+    public class ChartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
