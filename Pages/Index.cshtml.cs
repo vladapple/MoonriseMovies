c3f3f788afb8436f2ae8743987c2a81c3c3bc90b
GermanSamosa <mat_rao@hotmail.com>
2022-11-07 12:54:46 -0500

    added screenings schedule to viewMovie page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -15,7 +15,12 @@ public class IndexModel : PageModel
         _logger = logger;
         this.db = db;
     }
+    [BindProperty(SupportsGet = true)]
+        public int Id { get; set; }
     public List<Screening> Screenings {get; set;}
+    public List<Screening> Friday = new List<Screening>();
+    public List<Screening> Saturday = new List<Screening>();
+    public List<Screening> Sunday = new List<Screening>();
     public List<Movie> Movies {get; set;}
     public Movie FeaturedMovie {get; set;}
     public async Task OnGetAsync()
@@ -29,8 +34,22 @@ public class IndexModel : PageModel
         string imageBase64Data = Convert.ToBase64String(bytes);
         string imageDataURL = string.Format("data:image/jpg;base64,{0}", imageBase64Data);
         ViewData["Image"+FeaturedMovie.Id.ToString()] = imageDataURL;
+
+        Screenings = await db.Screenings.ToListAsync();
+        foreach(var s in Screenings)
+        {
+            if(s.Start.DayOfWeek.Equals(DayOfWeek.Friday))
+            {
+                Friday.Add(s);
+            }
+            else if(s.Start.DayOfWeek.Equals(DayOfWeek.Saturday))
+            {
+                Saturday.Add(s);
+            }
+            else if(s.Start.DayOfWeek.Equals(DayOfWeek.Sunday))
+            {
+                Sunday.Add(s);
+            }
+        }
     }
-
-   
-
 }
