bcc3d8ce337f0297d186a605de6ecd11388256f2
GermanSamosa <mat_rao@hotmail.com>
2022-11-07 18:08:47 -0500

    orderpage problems

--- Pages/OrderPage.cshtml.cs
+++ Pages/OrderPage.cshtml.cs
@@ -1,12 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MoonriseMovies.Data;
+using MoonriseMovies.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
+
 
 namespace MoonriseMovies.Pages
 {
     public class OrderPageModel : PageModel
     {
-        public void OnGet()
+        private MoonriseDBContext db;
+        public OrderPageModel(MoonriseDBContext db)
         {
+            this.db = db;
         }
+        [BindProperty(SupportsGet = true)]
+        public int Id { get; set; }
+        public Ticket ticket {get; set;}
+        public Movie movie {get;set;}
+        public Screening screening {get;set;}
+        public IdentityUser user {get; set;}
+        public async Task OnGetAsync()
+        {
+            screening = await db.Screenings.FindAsync(Id);
+
+            movie = await db.Movies.FindAsync(screening.Movie);
+
+            byte[] bytes;
+
+            // bytes = movie.Image;
+            // string imageBase64Data = Convert.ToBase64String(bytes);
+            // string imageDataURL = string.Format("data:image/jpg;base64,{0}", imageBase64Data);
+            // ViewData["Image" + movie.Id.ToString()] = imageDataURL;
+        }
+        
     }
 }
