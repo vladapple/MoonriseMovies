6d37ad960ac28ff4b6c577b1ba45c16903dc0267
mitchellfridman <fridmanmitchell@gmail.com>
2022-11-06 19:33:52 -0500

    everything I did is commented out

--- Pages/Profile.cshtml.cs
+++ Pages/Profile.cshtml.cs
@@ -1,10 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MoonriseMovies.Models;
+using MoonriseMovies.Data;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace MOONRISEMOVIES.Pages
 {
     public class ProfileModel : PageModel
-    {
+    {/*
+        private MoonriseDBContext db;
+        private readonly UserManager<IdentityUser> _userManager;
+        public UserEditModel(MoonriseDBContext db, UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+            this.db = db;
+        }
+        public IdentityUser user{ get; set; }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            user = await db.Users.FindAsync(Id);
+            user = await _userManager.ChangePasswordAsync(user, "oldpassword", "newpassword");
+        }*/
+
         public void OnGet()
         {
         }
