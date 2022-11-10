21413a20fce46d0932e49b1742f299d23d2a3622
Vlad Epel <vladislav.epelboym@johnabbottcollege.net>
2022-11-09 23:46:03 -0500

    charts

--- Pages/Admin/Chart.cshtml.cs
+++ Pages/Admin/Chart.cshtml.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MoonriseMovies.Models;
+using MoonriseMovies.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MoonriseMovies.Pages.Admin
 {
     public class ChartModel : PageModel
     {
-        public void OnGet()
+        private readonly MoonriseDBContext db;
+
+        public ChartModel(MoonriseDBContext db)
         {
+            this.db = db;
+        }
+        public List<Movie> movieList { get; set; } = new List<Movie>();
+        public async Task OnGetAsync()
+        {
+            movieList = await db.Movies.ToListAsync();  
         }
     }
 }
